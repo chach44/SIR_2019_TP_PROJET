88bbf6f1559261e9ee698c37d731c1868128a74a
18001066 <18001066@e107m09.istic.univ-rennes1.fr>
2019-01-17 17:07:50 +0100

    Getter setter

--- src.main.java/priseRdv/Nourriture.java
+++ src.main.java/priseRdv/Nourriture.java
@@ -2,7 +2,20 @@ package priseRdv;
 
 public class Nourriture {
 
+	
 	public int idAliment;
 	public String nomAliment;
+	public int getIdAliment() {
+		return idAliment;
+	}
+	public void setIdAliment(int idAliment) {
+		this.idAliment = idAliment;
+	}
+	public String getNomAliment() {
+		return nomAliment;
+	}
+	public void setNomAliment(String nomAliment) {
+		this.nomAliment = nomAliment;
+	}
 	
 }
