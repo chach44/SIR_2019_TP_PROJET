9ff63c5888249193cb129574f4ae8d48084d7e2f
18001066 <18001066@e107m09.istic.univ-rennes1.fr>
2019-01-17 17:35:56 +0100

    Reunion

--- src.main.java/priseRdv/Reunion.java
+++ src.main.java/priseRdv/Reunion.java
@@ -29,5 +29,77 @@ public class Reunion {
 	
 	@ManyToOne
 	Sondages s;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getIntitule() {
+		return intitule;
+	}
+
+	public void setIntitule(String intitule) {
+		this.intitule = intitule;
+	}
+
+	public String getResume() {
+		return resume;
+	}
+
+	public void setResume(String resume) {
+		this.resume = resume;
+	}
+
+	public String getLieu() {
+		return lieu;
+	}
+
+	public void setLieu(String lieu) {
+		this.lieu = lieu;
+	}
+
+	public String getClearCode() {
+		return clearCode;
+	}
+
+	public void setClearCode(String clearCode) {
+		this.clearCode = clearCode;
+	}
+
+	public boolean isPause() {
+		return pause;
+	}
+
+	public void setPause(boolean pause) {
+		this.pause = pause;
+	}
+
+	public Collection<Participant> getParticipantPresent() {
+		return ParticipantPresent;
+	}
+
+	public void setParticipantPresent(Collection<Participant> participantPresent) {
+		ParticipantPresent = participantPresent;
+	}
+
+	public Collection<Participant> getParticipantAbsent() {
+		return ParticipantAbsent;
+	}
+
+	public void setParticipantAbsent(Collection<Participant> participantAbsent) {
+		ParticipantAbsent = participantAbsent;
+	}
+
+	public Sondages getS() {
+		return s;
+	}
+
+	public void setS(Sondages s) {
+		this.s = s;
+	}
 	
 }
