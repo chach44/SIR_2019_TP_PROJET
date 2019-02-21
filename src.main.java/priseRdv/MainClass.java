87f3611cf741ad2180bfc77d77ec07c609b66f98
Sophie <Sophie@DESKTOP-B7CRI7I.home>
2019-02-21 14:29:34 +0100

    Ajout d'une main classe pour rendre les objets persistants

---
+++ src.main.java/priseRdv/MainClass.java
@@ -0,0 +1,11 @@
+package priseRdv;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Participant p1 = new Participant("Laurensan", "Charlotte ","Laurensan.Charlotte@gmail.com")
+		
+	}
+
+}
