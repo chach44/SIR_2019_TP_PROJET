e6371478dd4cdf63b741087eb4e64ecb49e6e3f9
Sophie <Sophie@10.41.38.233>
2019-02-27 10:49:24 +0100

    Changement JpaTest fix bug

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import javax.persistence.*;
 
-import Participant;
+import priseRdv.Participant;
 import priseRdv.Question;
 import priseRdv.Reunion;
 import priseRdv.Sondages;
