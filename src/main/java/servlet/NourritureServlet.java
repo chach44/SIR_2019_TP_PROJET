c53aa3aed50fe7879ed8a30fa58df979214aa2c1
Sophie <Sophie@DESKTOP-B7CRI7I>
2019-03-05 23:24:55 +0100

    Nourriture servelet

--- src/main/java/servlet/NourritureServlet.java
+++ src/main/java/servlet/NourritureServlet.java
@@ -16,7 +16,21 @@ public class NourritureServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-            
+        
+    	
+    	 PrintWriter out = resp.getWriter();
+    	 int intDate = 1;
+    	 int intQuestion = 1;
+  	 
+    	    out.println("<HTML>\n<BODY>\n" +
+    	                "<H1>Nouriture</H1>\n" + 
+						"<FORM Method=\"POST\" Action=\"/Nourriture\">"+
+						"Nom de l'aliment: <INPUT type=text size=30 name=nom><BR>"+
+						"<INPUT type=submit value=Send>"+
+						"</FORM>  "      +
+			    	    "</BODY></HTML>");
+    	
+    	
         PrintWriter p = new PrintWriter(resp.getOutputStream());
         p.print("Nourriture");
         p.flush();
@@ -27,6 +41,17 @@ public class NourritureServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         // TODO Auto-generated method stub
-        super.doPost(req, resp);
+    	resp.setContentType("text/html");
+
+		PrintWriter out = resp.getWriter();
+
+    
+		out.println("<HTML>\n<BODY>\n" +
+		            "<H1>Recapitulatif de l'ajout de type de nourriture</H1>\n" +
+		            "<UL>\n" +            
+		    " <LI>Nom: "
+		            + req.getParameter("nom") + "\n" +
+		                
+		    "</BODY></HTML>");
     }    
 }
